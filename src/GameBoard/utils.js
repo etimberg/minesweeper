ea202470d824c96e069a1c8f23e0c6a9ea3aa9ad
Evert Timberg <evert.timberg+github@gmail.com>
2019-11-02 10:55:59 -0400

    Remove console log

--- src/GameBoard/utils.js
+++ src/GameBoard/utils.js
@@ -2,10 +2,6 @@ export function indexToRowCol(index, width) {
   // Return the 0 based row/col to place this index at
   const row = Math.floor(index / width);
   const col = index % width;
-
-  if (col < 0) {
-    console.log(`${row} ${col} ${index}`);
-  }
   return [row, col]; 
 }
 
